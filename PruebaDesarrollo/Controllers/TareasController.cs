9f013be89bb03f79d5af5eb0294a4f95e4e437b9
Erwin <erwin.paralesda@gmail.com>
2024-02-07 12:08:43 -0500

    Add files via upload

---
+++ PruebaDesarrollo/Controllers/TareasController.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using PruebaDesarrollo.Models;
+
+namespace PruebaDesarrollo.Controllers
+{
+    public class TareasController : ApiController
+    {
+        private TareasDbContext db = new TareasDbContext();
+
+        // GET: api/Tareas
+        public IQueryable<Tarea> GetTareas()
+        {
+            return db.Tareas;
+        }
+
+        // GET: api/Tareas/5
+        [ResponseType(typeof(Tarea))]
+        public IHttpActionResult GetTarea(int id)
+        {
+            Tarea tarea = db.Tareas.Find(id);
+            if (tarea == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(tarea);
+        }
+
+        // PUT: api/Tareas/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutTarea(int id, Tarea tarea)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            tarea.Id = id;
+
+
+            db.Entry(tarea).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!TareaExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Tareas
+        [ResponseType(typeof(Tarea))]
+        public IHttpActionResult PostTarea(Tarea tarea)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Tareas.Add(tarea);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = tarea.Id }, tarea);
+        }
+
+        // DELETE: api/Tareas/5
+        [ResponseType(typeof(Tarea))]
+        public IHttpActionResult DeleteTarea(int id)
+        {
+            Tarea tarea = db.Tareas.Find(id);
+            if (tarea == null)
+            {
+                return NotFound();
+            }
+
+            db.Tareas.Remove(tarea);
+            db.SaveChanges();
+
+            return Ok(tarea);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool TareaExists(int id)
+        {
+            return db.Tareas.Count(e => e.Id == id) > 0;
+        }
+    }
+}
---
+++ PruebaDesarrollo/Models/Tarea.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+
+namespace PruebaDesarrollo.Models
+{
+    public class Tarea
+    {
+        public int Id { get; set; }
+        public string DescripcionTarea { get; set; }
+
+    }
+
+    class TareasDbContext : DbContext
+    {
+        public DbSet<Tarea> Tareas { get; set; }
+
+    }
+}
